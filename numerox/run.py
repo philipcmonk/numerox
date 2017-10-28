21d93a6101401fbab2440058b20b92ffc451b2d5
Keith Goodman <kwgoodman@gmail.com>
2017-10-28 16:21:38 -0700

    rough draft of production and backtest functions

--- numerox/run.py
+++ numerox/run.py
@@ -1,4 +1,4 @@
-from numerox import Prediction
+from numerox import Prediction, tournament_splitter, cv_splitter
 
 
 def run(model, data, splitter, prediction=None, verbosity=2):
@@ -7,4 +7,16 @@ def run(model, data, splitter, prediction=None, verbosity=2):
     for data_fit, data_predict in splitter:
         yhat = model.fit_predict(data_fit, data_predict)
         prediction.append(data_predict.ids, yhat)
+    return prediction
+
+
+def production(model, data):
+    splitter = tournament_splitter(data)
+    prediction = run(model, data, splitter)
+    return prediction
+
+
+def backtest(model, data, kfold=5, seed=0):
+    splitter = cv_splitter(data, kfold=kfold, seed=seed)
+    prediction = run(model, data, splitter)
     return prediction
