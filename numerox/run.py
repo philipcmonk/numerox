d9f9d575933f482239e4e576eb43c5a17464b699
Keith Goodman <kwgoodman@gmail.com>
2017-10-28 16:27:47 -0700

    redesign of main run function

--- numerox/run.py
+++ numerox/run.py
@@ -1,9 +1,8 @@
 from numerox import Prediction, tournament_splitter, cv_splitter
 
 
-def run(model, data, splitter, prediction=None, verbosity=2):
-    if prediction is None:
-        prediction = Prediction()
+def run(model, data, splitter, verbosity=2):
+    prediction = Prediction()
     for data_fit, data_predict in splitter:
         yhat = model.fit_predict(data_fit, data_predict)
         prediction.append(data_predict.ids, yhat)
